2f03952494bf5844d6a3e5454af93df166c61cde
bendunhampdx <bendunhampdx@gmail.com>
2022-07-24 14:39:49 -0700

    update music and photo sizing

--- src/components/Music/index.jsx
+++ src/components/Music/index.jsx
@@ -6,19 +6,33 @@ const Music = () => {
   return (
     <Container className={styles.outer}>
       <Row>
-        <div className={styles.meta}>
-          <p>Auditory Hallucinations</p>
-          <Col>
-            <div className={styles.embed_responsive}>
-              <iframe
-                src="https://open.spotify.com/embed/artist/4jPVcBmNQZbnSfMXLPWwGc?utm_source=generator"
-                frameBorder="0"
-                allowfullscreen=""
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              ></iframe>
-            </div>
-          </Col>
-        </div>
+        {/* <div className={styles.meta}> */}
+        <p>Auditory Hallucinations</p>
+        <Col sm={12} md={6}>
+          <div className={styles.embed_responsive}>
+            <iframe
+              src="https://bandcamp.com/EmbeddedPlayer/album=959422774/size=large/bgcol=ffffff/linkcol=333333/artwork=small/transparent=true/"
+              seamless
+            >
+              <a href="https://disassturbator.bandcamp.com/album/next-thing-i-knew">
+                Next Thing I knew... by Disassturbator
+              </a>
+            </iframe>
+          </div>
+        </Col>
+        <Col sm={12} md={6}>
+          <div className={styles.embed_responsive}>
+            <iframe
+              src="https://bandcamp.com/EmbeddedPlayer/album=647975473/size=large/bgcol=ffffff/linkcol=333333/artwork=small/transparent=true/"
+              seamless
+            >
+              <a href="https://disassturbator.bandcamp.com/album/nothing-since-forever-ep">
+                Nothing Since Forever EP by Disassturbator
+              </a>
+            </iframe>
+          </div>
+        </Col>
+        {/* </div> */}
       </Row>
     </Container>
   );
